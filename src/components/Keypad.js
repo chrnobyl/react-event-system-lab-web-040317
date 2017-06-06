fc93384967333f3dc456ab50ea47db2a7dc80436
github username <github email address>
2017-06-06 14:03:06 -0400

    Done

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,21 @@
 // Code Keypad Component Here
+import React from 'react'
+import PropTypes from 'prop-types'
+
+class Keypad extends React.Component {
+  pw = () => {
+   console.log('Entering password...');
+ }
+
+  render() {
+    return (
+      <input onKeyUp={this.pw} type="password" />
+    )
+  }
+}
+
+Keypad.PropTypes = {
+  password: PropTypes.string
+}
+
+export default Keypad;
